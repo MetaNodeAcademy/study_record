3144dd94f08addb8a90bbff81a3bccd7a0d503ae
dancy0473 <hjsdbajhdb@163.com>
2025-09-16 20:06:11 +0800

    重构回文数检查逻辑，添加输入参数并修复输出逻辑

--- go_file/go_homework/main.go
+++ go_file/go_homework/main.go
@@ -1,12 +1,16 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/MetaNodeAcademy/study_record/tree/main/go_file/go_homework/palindromicnumber"
 	"github.com/MetaNodeAcademy/study_record/tree/main/go_file/go_homework/validparentheses"
 )
 
 func main() {
-	palindromicnumber.IsPalindrome()
+	var x int
+	fmt.Scan(&x)
+	fmt.Println(palindromicnumber.IsPalindrome(x))
 	validparentheses.IsValid()
 	return
 }
--- go_file/go_homework/palindromicnumber/palindromicnumber.go
+++ go_file/go_homework/palindromicnumber/palindromicnumber.go
@@ -1,49 +1,68 @@
 package palindromicnumber
 
-import "fmt"
-
 // 回文数
-func IsPalindrome() {
-	var str string
-	var num int
-	var strlen int
-	for {
-		fmt.Scan(&str)
-		strlen = len(str)
-		num = strlen
-		if num%2 == 0 {
-			num = num / 2
-		} else {
-			num = num/2 + 1
-		}
-		var char rune
-		for i, v := range str {
-			is := false
-			index := strlen
-			for _, vv := range str {
-				index--
-				if index < num {
-					is = false
-					break
-				}
-				if index == i {
-					char = vv
-					is = true
-					break
-				}
-			}
-			if is {
-				if v != char {
-					fmt.Print("false")
-					break
-				} else {
-					if i == num-1 {
-						fmt.Print("true")
-						break
-					}
-				}
-			}
+func IsPalindrome(x int) bool {
+	var input int
+	ispalindrome := false
+	if x < 0 {
+		input = -x
+	}
+	num := 0
+	for i := 0; ; i++ {
+		if 0 < input%(10^i) < 10 {
+			num = i
+			break
 		}
 	}
-	return
+	for i := 0; ; i++ {
+		if i == num {
+			ispalindrome = true
+			break
+		}
+		if input%(10^i) != input%(10^num) {
+			ispalindrome = false
+			break
+		}
+	}
+
+	return ispalindrome
+	// var str string
+	// var num int
+	// var strlen int
+	// for {
+	// 	fmt.Print("请输入一个字符串：")
+	// 	fmt.Scan(&str)
+	// 	str = strings.TrimLeft(str, "+-")
+	// 	strlen = len(str)
+	// 	num = strlen
+	// 	if num%2 == 0 {
+	// 		num = num / 2
+	// 	} else {
+	// 		num = num/2 + 1
+	// 	}
+	// 	var char rune
+	// 	for i, v := range str {
+	// 		is := false
+	// 		index := strlen
+	// 		for _, vv := range str {
+	// 			index--
+	// 			if index == i {
+	// 				char = vv
+	// 				is = true
+	// 				break
+	// 			}
+	// 		}
+	// 		if is {
+	// 			if v != char {
+	// 				fmt.Print("false")
+	// 				break
+	// 			} else {
+	// 				if i == num-1 {
+	// 					fmt.Print("true")
+	// 					break
+	// 				}
+	// 			}
+	// 		}
+	// 	}
+	// }
 }
