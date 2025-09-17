afbe84d537b160ba090ba92820e1323725f6cecc
dancy0473 <hjsdbajhdb@163.com>
2025-09-17 17:34:43 +0800

    更新主函数，修改输入选择为最长公共前缀并添加相关逻辑

--- go_file/go_homework/main.go
+++ go_file/go_homework/main.go
@@ -1,15 +1,19 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
+	"strings"
 
+	"github.com/MetaNodeAcademy/study_record/tree/main/go_file/go_homework/longestcommonprefix"
 	"github.com/MetaNodeAcademy/study_record/tree/main/go_file/go_homework/palindromicnumber"
 	"github.com/MetaNodeAcademy/study_record/tree/main/go_file/go_homework/validparentheses"
 )
 
 func main() {
 	var num int
-	num = 2
+	num = 3
 	switch num {
 	case 1:
 		//回文数
@@ -21,6 +25,13 @@ func main() {
 		var str string
 		fmt.Scan(&str)
 		fmt.Println(validparentheses.IsValid(str))
+	case 3:
+		// 最长公共前缀
+		reader := bufio.NewReader(os.Stdin)
+		line, _ := reader.ReadString('\n')
+		line = strings.TrimSpace(line)   // 去掉换行
+		strs := strings.Split(line, " ") // 按空格拆分
+		fmt.Println(longestcommonprefix.LongestCommonPrefix(strs))
 	default:
 		fmt.Println("无效输入")
 
